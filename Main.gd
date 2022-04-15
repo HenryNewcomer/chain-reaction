96a59837996f539e147825d138568cadb1efb526
Henry Newcomer <a.cliche.email@gmail.com>
2022-04-15 10:05:23 -0500

    Buttons flip images depending on if on/off

--- Main.gd
+++ Main.gd
@@ -1,24 +1,26 @@
 extends Node2D
 
-export var init_grid_size = Vector2(4, 4)
-var current_grid_size = Vector2.ZERO
-var current_grid_state = []
-
-func reset():
-	generate_grid(init_grid_size)
+export var grid_size = Vector2(4, 4)
+var current_grid = []
 
 func _ready():
 	Signals.connect("prion_clicked", self, "capture_current_grid_state")
-	reset()
+	reset_grid()
+	current_grid[2] = 0
+	draw_grid()
 
-func generate_grid(size_2d):
-	current_grid_size = size_2d
-	var columns = size_2d.x
-	var rows = size_2d.y
-	print("SETTING: %s by %s" % [columns, rows])
-	var i = 0
+func reset_grid():
+	var converted_grid_size = grid_size.x * grid_size.y
+	for i in range(converted_grid_size):
+		# Set all values to 1.
+		current_grid.append(1)
+
+func draw_grid():
+	var columns = grid_size.x
+	var rows = grid_size.y
 	var width = $Prion.rect_size.x
 	var height = $Prion.rect_size.y
+	var i = 0
 	
 	for c in columns:
 		for r in rows:
@@ -27,9 +29,10 @@ func generate_grid(size_2d):
 			add_child(new_inst)
 			new_inst.rect_position = Vector2(c*width, r*height)
 			print("%s) %s, %s" % [i, c, r])
-			current_grid_state.push_back(1)
+			current_grid.push_back(1)
+	print(current_grid)
 
 func capture_current_grid_state():
 	print("[ CAPTURING STATE! ]")
-	print(current_grid_state)
+	print(current_grid)
 	pass
